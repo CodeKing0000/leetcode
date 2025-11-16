48dda627e7fdcf8e1b1beb13c608482085a2bb9e
CodeKing0000 <tymofey54@gmail.com>
2025-11-16 15:39:12 +0700

    Update p.cpp

--- p.cpp
+++ p.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cctype>
 
-void foo(std::string str) {
+void foo(const std::string& str) {
     int p = str.length() /2;
 
     std::string a = "";
@@ -10,7 +10,7 @@ void foo(std::string str) {
     for(int i = 0; i < p; i++) {
         a.push_back(str[i]);
     }
-    for(int i = p; i <= str.length(); i++) {
+    for(int i = p; i <= str.length() - 1; i++) {
         b.push_back(str[i]);
     }
     int result_1 = 0;
@@ -18,16 +18,12 @@ void foo(std::string str) {
     for(int i = 0; i < a.length(); i++) {
         if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U') {
             result_1++;
-            
         }
-        // std::cout << result_1 << " " << str[i] << "| ";
     }
     for(int i = 0; i < b.length(); i++) {
         if(b[i] == 'a' || b[i] == 'e' || b[i] == 'i' || b[i] == 'o' || b[i] == 'u' || b[i] == 'A' || b[i] == 'E' || b[i] == 'I' || b[i] == 'O' || b[i] == 'U') {
-            result_2++;
-             
+            result_2++;   
         }
-        std::cout << result_2 << " " << str[i] << "| ";
     }
 
     if(result_1 == result_2) {
@@ -42,3 +38,4 @@ int main() {
     std::string str = "ghfj";
     foo(str);
 }
+
