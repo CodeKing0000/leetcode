b5e881a674e542ec13b386c18f13ccca763ed17d
CodeKing0000 <tymofey54@gmail.com>
2025-11-30 21:29:32 +0700

    Add files via upload

--- p.cpp
+++ p.cpp
@@ -1,41 +1,16 @@
-#include <iostream>
-#include <string>
-#include <cctype>
-
-void foo(const std::string& str) {
-    int p = str.length() /2;
-
-    std::string a = "";
-    std::string b = "";
-    for(int i = 0; i < p; i++) {
-        a.push_back(str[i]);
-    }
-    for(int i = p; i <= str.length() - 1; i++) {
-        b.push_back(str[i]);
-    }
-    int result_1 = 0;
-    int result_2 = 0;
-    for(int i = 0; i < a.length(); i++) {
-        if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U') {
-            result_1++;
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        std::queue<int> students_q
+        for(int i = 0; i < students.size() - 1; ++i) {
+            for(int j = 0; j < sandwiches.size() - 1; ++j) {
+                if(i != j) {
+                    students_q.push(i)
+                }
+            }
+        }
+        if(students_q.push(i).empty() == true) {
+            
         }
     }
-    for(int i = 0; i < b.length(); i++) {
-        if(b[i] == 'a' || b[i] == 'e' || b[i] == 'i' || b[i] == 'o' || b[i] == 'u' || b[i] == 'A' || b[i] == 'E' || b[i] == 'I' || b[i] == 'O' || b[i] == 'U') {
-            result_2++;   
-        }
-    }
-
-    if(result_1 == result_2) {
-        std::cout << "true";
-    }
-    else {
-        std::cout << "false";
-    }
-}
-
-int main() {
-    std::string str = "ghfj";
-    foo(str);
-}
-
+};
